c124e48543a97da52bb1514c117bad93fb2bd128
Dinh Nguyen <dinguyen@opensource.altera.com>
2015-12-01 10:48:36 -0600

    arm: socfpga: arria10: add board files for the Arria10 SoCDK
    Add minimal support for the Arria10 SoCDK.
    
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>

---
+++ board/altera/arria10-socdk/socfpga.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015 Altera Corporation <www.altera.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#include <common.h>
+#include <asm/io.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void s_init(void)
+{
+}
+
+/*
+ * Miscellaneous platform dependent initialisations
+ */
+int board_init(void)
+{
+	/* Address of boot parameters for ATAG (if ATAG is used) */
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
+	return 0;
+}
