1d23e240b4da4d8f1059f2ecc3cb68573d2d07a4
Dinh Nguyen <dinguyen@kernel.org>
2016-07-12 17:20:35 -0500

    Edit pl330.h

--- arch/arm/include/asm/pl330.h
+++ arch/arm/include/asm/pl330.h
@@ -9,23 +9,6 @@
 #ifndef	__PL330_H_
 #define	__PL330_H_
 
-struct dma_pl330_platdata {
-	/*
-	 * Number of valid peripherals connected to DMAC.
-	 * This may be different from the value read from
-	 * CR0, as the PL330 implementation might have 'holes'
-	 * in the peri list or the peri could also be reached
-	 * from another DMAC which the platform prefers.
-	 */
-	u8 nr_valid_peri;
-	/* Array of valid peripherals */
-	u8 *peri_id;
-	/* Operational capabilities */
-	dma_cap_mask_t cap_mask;
-	/* Bytes to allocate for MC buffer */
-	unsigned mcbuf_sz;
-};
-
 /* structure to be passed in for pl330_transfer_x */
 struct pl330_transfer_struct {
 	u32 channel_num;
