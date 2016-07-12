197ab59fe2e321fcc1c8ee7a480dd30563417da4
Dinh Nguyen <dinguyen@kernel.org>
2016-07-12 16:59:13 -0500

    Edit pl330.h

--- arch/arm/include/asm/pl330.h
+++ arch/arm/include/asm/pl330.h
@@ -3,16 +3,11 @@
  * Copyright (C) 2010 Samsung Electronics Co. Ltd.
  *	Jaswinder Singh <jassi.brar@samsung.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef	__AMBA_PL330_H_
-#define	__AMBA_PL330_H_
-
-#include <linux/dmaengine.h>
+#ifndef	__PL330_H_
+#define	__PL330_H_
 
 struct dma_pl330_platdata {
 	/*
@@ -31,5 +26,19 @@ struct dma_pl330_platdata {
 	unsigned mcbuf_sz;
 };
 
-extern bool pl330_filter(struct dma_chan *chan, void *param);
-#endif	/* __AMBA_PL330_H_ */
+/* structure to be passed in for pl330_transfer_x */
+struct pl330_transfer_struct {
+	u32 channel_num;
+	u32 src_addr;
+	u32 dst_addr;
+	u32 size_byte;
+	u32 brst_size;
+	u32 single_brst_size;
+	u32 brst_len;
+	u32 peripheral_id;
+	u32 transfer_type;
+	u32 enable_cache1;
+	u32 buf_size;
+	u8 *buf;
+};/* structure to be passed in for pl330_transfer_x */
+#endif	/* __PL330_H_ */
