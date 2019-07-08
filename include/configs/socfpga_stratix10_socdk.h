altera-opensource/u-boot-socfpga

commit fffd8a34fefef208c4ca98a64c21eca38179b8d2

arm: socfpga: Ensure the DTS QSPI clock is set correctly
The clocking section of Linux device tree was changed between
4.9.78-ltsi and 4.14.73-ltsi. This patch ensures that QSPI clock can be
set correctly despite of different clocking section location in both
Linux 4.9.78-ltsi and 4.14.73-ltsi.

Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

include/configs/socfpga_stratix10_socdk.h
@@ -121,8 +121,10 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 		"echo Enabling QSPI at Linux DTB...;" \
 		"fdt addr ${fdt_addr}; fdt resize;" \
 		"fdt set /soc/spi@ff8d2000 status okay;" \
-		"fdt set /soc/clkmgr/clocks/qspi_clk clock-frequency " \
-		" ${qspi_clock}; fi; \0" \
+		"if fdt set /soc/clocks/qspi-clk clock-frequency;" \
+		" ${qspi_clock};" \
+		" else fdt set /soc/clkmgr/clocks/qspi_clk clock-frequency;" \
+		" ${qspi_clock}; fi\0" \
 	"scriptaddr=0x02100000\0" \
 	"scriptfile=u-boot.scr\0" \
 	"fatscript=if fatload mmc 0:1 ${scriptaddr} ${scriptfile};" \

altera-opensource/u-boot-socfpga-next

commit 1f580c28ce34c7716591c0142df49212957b095e

arm: socfpga: Fixing undefined commands by removing ";"
Fixes: fffd8a34fefe
("arm: socfpga: Ensure the DTS QSPI clock is set correctly")

Accidentally adding ";" before the end of command, so this patch is created
to remove the ";" from middle of command.

Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

include/configs/socfpga_stratix10_socdk.h
@@ -121,9 +121,9 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 		"echo Enabling QSPI at Linux DTB...;" \
 		"fdt addr ${fdt_addr}; fdt resize;" \
 		"fdt set /soc/spi@ff8d2000 status okay;" \
-		"if fdt set /soc/clocks/qspi-clk clock-frequency;" \
+		"if fdt set /soc/clocks/qspi-clk clock-frequency" \
 		" ${qspi_clock};" \
-		" else fdt set /soc/clkmgr/clocks/qspi_clk clock-frequency;" \
+		" else fdt set /soc/clkmgr/clocks/qspi_clk clock-frequency" \
 		" ${qspi_clock}; fi\0" \
 	"scriptaddr=0x02100000\0" \
 	"scriptfile=u-boot.scr\0" \